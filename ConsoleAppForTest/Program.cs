dc8fc6b8e2f5afd0ee21a7a2ace03308d9f638a8
Dennis Velez <velez.dennis0@outlook.com>
2020-06-24 20:21:41 -0400

    Add project files.

---
+++ ConsoleAppForTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleAppForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            List<string> result = new List<string> { "Pedro" };
+            result.Add("Carlos");
+            result.Add("Ramon");
+
+            string concat = string.Join(",", result);
+            Console.WriteLine(concat);
+
+            string concat2 = "Pedro, Carlos, Ramon";
+
+            List<string> result2 = concat2.Split(',').ToList<string>();
+            Console.WriteLine(result2);
+
+
+        }
+    }
+}
