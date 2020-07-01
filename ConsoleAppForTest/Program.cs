6ecec8a648be4aa408505c0cab8433b7d9c6d588
Dennis Velez <velez.dennis0@outlook.com>
2020-06-30 21:38:28 -0400

    Example Modify Objecti List Properties Value

--- ConsoleAppForTest/Program.cs
+++ ConsoleAppForTest/Program.cs
@@ -23,7 +23,44 @@ namespace ConsoleAppForTest
             List<string> result2 = concat2.Split(',').ToList<string>();
             Console.WriteLine(result2);
 
+            //****************************
+            List<Customer.customermodel> customermodels = Customer.GetModel();
 
+            foreach (Customer.customermodel item in customermodels.Where(x=>x.name=="Rey").Select(x=>x))
+            {
+                item.name = "jose";
+            }
+
+            customermodels.Where(c => c.name =="Javier").Select(c => { c.name = "Pedro"; c.lastname="Garcia"; return c; }).ToList();
+
+
+
+        }
+
+        
+    }
+
+    public static class Customer
+    {
+        public static List<customermodel> GetModel()
+        {
+            List<customermodel> customers = new List<customermodel>
+            {
+                new customermodel { CustId = 1, name = "Javier", lastname = "Perez" },
+                new customermodel { CustId = 2, name = "Carlos", lastname = "Ramirez" },
+                new customermodel { CustId = 3, name = "Rey", lastname = "Ortiz" }
+
+            };
+            
+
+            return customers;
+        }
+
+        public class customermodel
+        {
+            public int CustId { get; set; }
+            public string name { get; set; }
+            public string lastname { get; set; }
         }
     }
 }
